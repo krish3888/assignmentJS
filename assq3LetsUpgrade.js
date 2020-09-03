a6fe3eb5f58e950ecb82ad05fa1ec6d77be7841b
Krishna Shahi <37612052+krish3888@users.noreply.github.com>
2020-09-03 23:33:22 +0530

    prompt is added

--- assq3LetsUpgrade.js
+++ assq3LetsUpgrade.js
@@ -1,11 +1,14 @@
 // Program to search for a element in a array of strings
 
-let sentence = prompt("enter string array").split(' ');
-let search = 'shahi';
+let sentence = prompt('enter array of string').split(' ');
+let search = prompt('element to be search');
 let count=0; 
-sentence.forEach(function(match) {
+let pos= new Array();
+sentence.forEach(function(match,ind) {
     if(search==match){
+        let k=0;
+        pos[count] = ++ind;
         count++;    
     }
 });
- console.log(`total occurance of "${search}" in "${sentence}" is "${count}"`);
+ console.log(`total occurance of "${search}" in "${sentence}" is "${count}" at position ${pos}`);
